bf4d880eeb15ad67f8a83ad382501bea9d57152f
Miguel Rios <miguelriosoliveira@gmail.com>
2021-03-21 19:42:33 -0300

    Second component: Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 	:root {
 		--red: #e52e4d;
+		--green: #33cc95;
 		--blue: #5429cc;
 		--blue-light: #6933ff;
 		--text-title: #363f5f;
@@ -32,7 +33,7 @@ export const GlobalStyle = createGlobalStyle`
 	}
 
 	body, input, textarea, button {
-		font-family: Poppins, sans-serif;
+		font-family: 'Poppins', sans-serif;
 		font-weight: 400;
 	}
 
