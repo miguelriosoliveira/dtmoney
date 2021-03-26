26da3b174f9ab6f97820587a6632af656e85074e
Miguel Rios <miguelriosoliveira@gmail.com>
2021-03-25 22:19:01 -0300

    Modal global styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,24 @@
 import Modal, { Props as ModalProps } from 'react-modal';
 
+import { Container } from './styles';
+
 export function NewTransactionModal({ isOpen, onRequestClose }: ModalProps) {
 	return (
-		<Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-			asdasdasd
+		<Modal
+			isOpen={isOpen}
+			onRequestClose={onRequestClose}
+			className="react-modal-content"
+			overlayClassName="react-modal-overlay"
+		>
+			<Container>
+				<h2>Cadastrar Transação</h2>
+
+				<input placeholder="Título" />
+				<input type="number" placeholder="Valor" />
+				<input placeholder="Categoria" />
+
+				<button type="submit">Cadastrar</button>
+			</Container>
 		</Modal>
 	);
 }
