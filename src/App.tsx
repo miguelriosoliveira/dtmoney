ec341202890c7303cc3cef815008c753544c9d04
Miguel Rios <miguelriosoliveira@gmail.com>
2021-03-30 22:12:52 -0300

    Created TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -20,7 +21,7 @@ export function App() {
 	}
 
 	return (
-		<>
+		<TransactionsContext.Provider value={[]}>
 			<GlobalStyle />
 			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 			<Dashboard />
@@ -28,6 +29,6 @@ export function App() {
 				isOpen={isNewTransactionModalOpen}
 				onRequestClose={handleCloseNewTransactionModal}
 			/>
-		</>
+		</TransactionsContext.Provider>
 	);
 }
